2d881efbb83105b571bde34c6705484e1ec084c1
odoo-dev <andrea.m.piovesana@gmail.com>
2019-11-24 22:17:18 +0100

    refactor openerp to odoo

--- xlsxwriter_report_sample/__manifest__.py
+++ xlsxwriter_report_sample/__manifest__.py
@@ -1,25 +1,5 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/python
-###############################################################################
-#
-# ODOO (ex OpenERP) 
-# Open Source Management Solution
-# Copyright (C) 2001-2018 Micronaet S.r.l. (<https://micronaet.com>)
-# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-# See the GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# Copyright 2019  Micronaet SRL (<http://www.micronaet.it>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Excel report Sample',
@@ -32,13 +12,11 @@
         'base',
         'product',
         'xlsxwriter_report',
-        ],
-    'data': [        
+    ],
+    'data': [
         'views/report_sample_view.xml',
-        ],
-    'demo': [],
-    'css': [],
+    ],
     'installable': True,
     'application': False,
     'auto_install': False,
-    }
+}
