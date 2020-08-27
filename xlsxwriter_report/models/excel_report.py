751d075eddf9df9af793050866037c2e964bebce
nicothebrush <nicola.riolini@gmail.com>
2020-08-27 11:43:27 +0200

    generic bugfix (fast message)

--- xlsxwriter_report/models/excel_report.py
+++ xlsxwriter_report/models/excel_report.py
@@ -454,6 +454,12 @@ class ExcelReport(models.TransientModel):
         """
         self._WS[ws_name].autofilter(*rectangle)
 
+    @api.model
+    def freeze_panes(self, ws_name, row, col):
+        """ Lock row or column
+        """
+        self._WS[ws_name].freeze_panes(row, col)
+
     # -------------------------------------------------------------------------
     # Image management:
     # -------------------------------------------------------------------------
