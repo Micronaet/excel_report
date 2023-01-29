31068e0bcf1346618cb0fcf184bcd78413a8179b
nicothebrush <nicola.riolini@gmail.com>
2023-01-29 10:34:30 +0100

    generic bugfix (fast message)

--- xlsxwriter_report/models/excel_report.py
+++ xlsxwriter_report/models/excel_report.py
@@ -197,7 +197,7 @@ class ExcelReport(models.TransientModel):
     def create(self, vals):
         """ Generate new WB when create record
         """
-        res = super(ExcelReport, self).with_context(prova='prova').create(vals)
+        res = super().with_context(prova='prova').create(vals)
         return res
 
     def get_b64_from_filename(self, workbook):
